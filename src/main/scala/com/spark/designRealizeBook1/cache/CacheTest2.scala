14d263eb1b8cddf8c7adb050c5838b6c965e0e43
Solarzhou <t-zhou@foxmail.com>
2022-05-15 15:55:48 +0800

    first update

--- src/main/scala/com/spark/designRealizeBook1/cache/CacheTest2.scala
+++ src/main/scala/com/spark/designRealizeBook1/cache/CacheTest2.scala
@@ -3,50 +3,50 @@ package com.spark.designRealizeBook1.cache
 import org.apache.spark.sql.SparkSession
 
 object CacheTest2 {
-  def main(args: Array[String]) {
+    def main(args: Array[String]) {
 
-    val spark = SparkSession
-      .builder()
-      .appName("Cache Test")
-      .master("local[2]")
-      .getOrCreate()
+        val spark = SparkSession
+            .builder()
+            .appName("Cache Test")
+            .master("local[2]")
+            .getOrCreate()
 
-    val sc = spark.sparkContext
+        val sc = spark.sparkContext
 
-    var inputRDD = sc.parallelize(Array[(Int, String)](
-      (1, "a"), (2, "b"), (3, "c"), (4, "d"), (5, "e"), (3, "f"), (2, "g"), (1, "h"), (2, "i")
-    ), 3)
+        var inputRDD = sc.parallelize(Array[(Int, String)](
+            (1, "a"), (2, "b"), (3, "c"), (4, "d"), (5, "e"), (3, "f"), (2, "g"), (1, "h"), (2, "i")
+        ), 3)
 
-    val mappedRDD = inputRDD.map(r => (r._1 + 1, r._2))
-    mappedRDD.cache()
+        val mappedRDD = inputRDD.map(r => (r._1 + 1, r._2))
+        mappedRDD.cache()
 
-    val reducedByKeyRDD = mappedRDD.reduceByKey((x, y) => x + "_" + y, 2)
+        val reducedByKeyRDD = mappedRDD.reduceByKey((x, y) => x + "_" + y, 2)
 
-    reducedByKeyRDD.cache()
-    println("------------reducedByKeyRDD----------")
-    reducedByKeyRDD.foreach(println)
-
-
-    val groupedByKeyRDD = mappedRDD.groupByKey().mapValues(v => v.toList)
-
-    groupedByKeyRDD.cache()
-    println("------------groupedByKeyRDD----------")
-    groupedByKeyRDD.foreach(println)
-    // mappedRDD.unpersist()
-
-    println(reducedByKeyRDD.toDebugString)
-    println(groupedByKeyRDD.toDebugString)
+        reducedByKeyRDD.cache()
+        println("------------reducedByKeyRDD----------")
+        reducedByKeyRDD.foreach(println)
 
 
-    val joinedRDD = reducedByKeyRDD.join(groupedByKeyRDD)
-    joinedRDD.mapPartitionsWithIndex((pid, iter)=>{
-      iter.map( value => "PID: " + pid + ", value: " + value)
-    }).foreach(println)
+        val groupedByKeyRDD = mappedRDD.groupByKey().mapValues(v => v.toList)
 
-    println("Join RDD")
-    println(joinedRDD.toDebugString)
-    System.in.read()
+        groupedByKeyRDD.cache()
+        println("------------groupedByKeyRDD----------")
+        groupedByKeyRDD.foreach(println)
+        // mappedRDD.unpersist()
 
-    spark.stop()
-  }
+        println(reducedByKeyRDD.toDebugString)
+        println(groupedByKeyRDD.toDebugString)
+
+
+        val joinedRDD = reducedByKeyRDD.join(groupedByKeyRDD)
+        joinedRDD.mapPartitionsWithIndex((pid, iter) => {
+            iter.map(value => "PID: " + pid + ", value: " + value)
+        }).foreach(println)
+
+        println("Join RDD")
+        println(joinedRDD.toDebugString)
+        System.in.read()
+
+        spark.stop()
+    }
 }
